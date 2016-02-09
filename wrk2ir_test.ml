bced4d68a31764863e33a6c438254f02bf2497d7
Anton Yabchinskiy <arn@bestmx.net>
2016-02-10 00:23:19 +0300

    wrk2ir_test: open Parser in lexer_tests

--- wrk2ir_test.ml
+++ wrk2ir_test.ml
@@ -14,12 +14,13 @@ let assert_tokens ~ctxt str toks =
   assert_equal ~ctxt (tokens str) toks
 
 let lexer_tests =
+  let open Parser in
   "Lexer" >::: [
     "Empty input" >:: (fun ctxt -> assert_tokens ~ctxt "" []);
     "Spaces" >:: (fun ctxt -> assert_tokens ~ctxt " 	  " []);
-    "Number" >:: (fun ctxt -> assert_tokens ~ctxt "0123" [Parser.NUMBER 123]);
+    "Number" >:: (fun ctxt -> assert_tokens ~ctxt "0123" [NUMBER 123]);
     "Name" >:: (fun ctxt -> assert_tokens
-                   ~ctxt " \"xyz foo %%%\"" [Parser.NAME "xyz foo %%%"])
+                   ~ctxt " \"xyz foo %%%\"" [NAME "xyz foo %%%"])
   ]
 
 let () = run_test_tt_main
