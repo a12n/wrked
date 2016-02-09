99b576aa977a86aac1c77bf7a652f858158335e8
Anton Yabchinskiy <arn@bestmx.net>
2016-02-10 00:21:09 +0300

    wrk2ir_test: some lexer tests

--- wrk2ir_test.ml
+++ wrk2ir_test.ml
@@ -1,8 +1,25 @@
+open Batteries
 open OUnit2
+
+let tokens str =
+  let buf = Lexing.from_string str in
+  Enum.from
+    (fun () ->
+       try Lexer.read buf
+       with End_of_file ->
+         raise Enum.No_more_elements) |>
+  List.of_enum
+
+let assert_tokens ~ctxt str toks =
+  assert_equal ~ctxt (tokens str) toks
 
 let lexer_tests =
   "Lexer" >::: [
-    (* TODO *)
+    "Empty input" >:: (fun ctxt -> assert_tokens ~ctxt "" []);
+    "Spaces" >:: (fun ctxt -> assert_tokens ~ctxt " 	  " []);
+    "Number" >:: (fun ctxt -> assert_tokens ~ctxt "0123" [Parser.NUMBER 123]);
+    "Name" >:: (fun ctxt -> assert_tokens
+                   ~ctxt " \"xyz foo %%%\"" [Parser.NAME "xyz foo %%%"])
   ]
 
 let () = run_test_tt_main
