622a2e4fa21da1911bfbd8a8192c307134367d9c
Anton Yabchinskiy <arn@bestmx.net>
2016-02-11 01:30:38 +0300

    Workout_repr: int32_caps function

--- workout_repr.ml
+++ workout_repr.ml
@@ -1,8 +1,15 @@
+open Batteries
+
 let from_lexbuf = Parser.workout Lexer.read
 
 let from_channel chan = from_lexbuf (Lexing.from_channel chan)
 
 module Ir = struct
+  let int32_caps =
+    ((List.fold_left Int32.add Int32.zero) %
+     (List.map Workout.Capability.to_int32)) %
+    Workout.caps
+
   let (%) chan str =
     output_string chan str;
     output_char chan '\n';
