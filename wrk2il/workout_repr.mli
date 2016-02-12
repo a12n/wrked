09ea5622ee5fb4cf3506a590a359287b7e189fc6
Anton Yabchinskiy <arn@bestmx.net>
2016-02-13 02:07:16 +0300

    Workout_repr: export from_lexbuf function

--- wrk2il/workout_repr.mli
+++ wrk2il/workout_repr.mli
@@ -2,6 +2,8 @@ open Batteries
 
 val from_channel : IO.input -> Workout.t
 
+val from_lexbuf : Lexing.lexbuf -> Workout.t
+
 val from_string : string -> Workout.t
 
 val to_channel : 'a IO.output -> Workout.t -> unit
