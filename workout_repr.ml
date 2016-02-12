24cdc0482481be2e0dd7eca71bb6e70b318199c6
Anton Yabchinskiy <arn@bestmx.net>
2016-02-12 11:44:33 +0300

    Workout_repr: output functions

--- workout_repr.ml
+++ workout_repr.ml
@@ -6,6 +6,20 @@ let from_channel = from_lexbuf % Lexing.from_channel
 
 let from_string = from_lexbuf % Lexing.from_string
 
+let to_channel chan {Workout.name; sport; steps} =
+  Option.may (fun name ->
+      Printf.fprintf chan "\"%s\": " name) name;
+  Option.may (fun sport ->
+      Printf.fprintf chan "%s, " (Workout.Sport.to_string sport)) sport;
+  IO.write chan '[';
+  List.iter (fun step -> ()) (Non_empty_list.to_list steps);
+  IO.write chan ']'
+
+let to_string w =
+  let chan = IO.output_string () in
+  to_channel chan w;
+  IO.close_out chan
+
 module Ir = struct
   let int32_caps =
     ((List.fold_left Int32.add Int32.zero) %
--- workout_repr.mli
+++ workout_repr.mli
@@ -4,6 +4,10 @@ val from_channel : IO.input -> Workout.t
 
 val from_string : string -> Workout.t
 
+val to_channel : 'a IO.output -> Workout.t -> unit
+
+val to_string : Workout.t -> string
+
 module Ir : sig
   val to_channel : 'a IO.output -> Workout.t -> unit
 end
