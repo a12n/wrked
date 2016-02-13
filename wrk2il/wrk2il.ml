2a62a4240025eddc634b4ea7a9421415487a67fa
Anton Yabchinskiy <arn@bestmx.net>
2016-02-13 13:59:18 +0300

    wrk2il: command line args to override workout name and sport fields

--- wrk2il/wrk2il.ml
+++ wrk2il/wrk2il.ml
@@ -1,6 +1,21 @@
 open Batteries
 
+let parse_args () =
+  let name = ref None in
+  let sport = ref None in
+  Arg.parse [ "-name", Arg.String (Ref.set name % Option.some),
+              " Override workout name";
+              "-sport", Arg.Symbol ([ "cycling"; "running";
+                                      "swimming"; "walking" ],
+                                     (Ref.set sport % Option.some %
+                                      Workout.Sport.of_string)),
+              " Override workout sport" ]
+    (fun _anon -> ())
+    "Translate workout description to intermediate language";
+  !name, !sport
+
 let () =
+  let _name, _sport = parse_args () in
   let lexbuf = Lexing.from_channel stdin in
   try
     Workout_repr.from_lexbuf lexbuf |>
