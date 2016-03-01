2c5a9aa1802144e5e2dac3825602e0f6935cf81d
Anton Yabchinskiy <arn@bestmx.net>
2016-03-01 14:24:38 +0300

    il2fit: import script to download FIT SDK

---
+++ il2fit/dl_sdk.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+FIT_VSN=16.60.0
+FIT_BASE=https://www.thisisant.com/assets/resources/FIT
+FIT_URL=$FIT_BASE/FitSDKRelease_$FIT_VSN.zip
+
+#-----------------------------------------------------------------------
+
+OUT_DIR=$1
+if [ -z "$OUT_DIR" ]; then
+    echo "Usage: $0 out_dir"
+    exit 1
+fi
+
+mkdir -p $OUT_DIR || exit 1
+
+TEMP_FILE=$(mktemp)
+trap "rm -f $TEMP_FILE" EXIT
+
+curl $FIT_URL -o $TEMP_FILE || exit 1
+unzip $TEMP_FILE -d $OUT_DIR || exit 1
+sed -i.orig '/#include <string>/a #include <cstring>' \
+    $OUT_DIR/cpp/fit.hpp || exit 1
