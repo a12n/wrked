df8d6d1d4ae2f293acbcb5a7c0dbde1970944fea
Anton Yabchinskiy <arn@bestmx.net>
2016-03-10 18:38:12 +0300

    Import xil2fit source (empty)
    Extended intermediate language, to create arbitrary FIT files.

---
+++ il2fit/xil2fit.cpp
@@ -0,0 +1,30 @@
+#include <ctime>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <fit_date_time.hpp>
+#include <fit_encode.hpp>
+#include <fit_file_creator_mesg.hpp>
+#include <fit_file_id_mesg.hpp>
+#include <fit_workout_mesg.hpp>
+#include <fit_workout_step_mesg.hpp>
+
+#ifndef _WITH_TESTS
+
+int main()
+{
+    // TODO
+    return 0;
+}
+
+#else  // _WITH_TESTS
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// TODO
+
+#endif  // _WITH_TESTS
