9512b8ebf14787df2b64f05ce65a630ac3e9b122
Anton Yabchinskiy <arn@bestmx.net>
2016-03-10 18:46:35 +0300

    xil2fit: add xil2fit function, create FIT encoder

--- il2fit/xil2fit.cpp
+++ il2fit/xil2fit.cpp
@@ -12,11 +12,45 @@
 #include <fit_workout_mesg.hpp>
 #include <fit_workout_step_mesg.hpp>
 
+namespace {
+
+//----------------------------------------------------------------------------
+// xil2fit
+
+void
+xil2fit(std::istream& input, std::iostream& output)
+{
+    fit::Encode encode;
+
+    encode.Open(output);
+
+    // TODO
+
+    if (!encode.Close()) {
+        throw std::runtime_error("FIT encoder failed");
+    }
+}
+
+} // namespace
+
+//----------------------------------------------------------------------------
+// Main
+
 #ifndef _WITH_TESTS
 
 int main()
 {
-    // TODO
+    std::stringstream output(std::ios::out | std::ios::binary);
+
+    try {
+        xil2fit(std::cin, output);
+    } catch (const std::exception& exn) {
+        std::cerr << exn.what() << std::endl;
+        return 1;
+    }
+
+    std::cout << output.str();
+
     return 0;
 }
 
