95be0d5aadce5fd729490dd62b5f8795d43fe416
Anton Yabchinskiy <arn@bestmx.net>
2016-03-23 16:50:46 +0300

    wrk2il: formatting in Non_empty_list

--- wrk2il/non_empty_list.ml
+++ wrk2il/non_empty_list.ml
@@ -1,7 +1,7 @@
 type 'a t = 'a * 'a list
 
 let of_list = function
-    h :: t -> h, t
-  | [] -> raise (Invalid_argument "Non_empty_list.of_list")
+  | x :: xs -> x, xs
+  | []      -> raise (Invalid_argument "Non_empty_list.of_list")
 
-let to_list (h, t) = h :: t
+let to_list (x, xs) = x :: xs
