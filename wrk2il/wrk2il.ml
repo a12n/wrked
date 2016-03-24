0a4b041bae813916dcf8496e0e731ee2ffcb4b56
Anton Yabchinskiy <arn@bestmx.net>
2016-03-24 11:43:44 +0300

    wrk2il: command line arg for translate/pretty-print mode

--- wrk2il/wrk2il.ml
+++ wrk2il/wrk2il.ml
@@ -3,16 +3,23 @@ open Batteries
 let parse_args () =
   let name = ref None in
   let sport = ref None in
+  let mode = ref Repr.Il.to_channel in
   Arg.parse [ "-name", Arg.String (Ref.set name % Option.some),
               " Override workout name";
               "-sport", Arg.Symbol ([ "cycling"; "running";
                                       "swimming"; "walking" ],
                                      (Ref.set sport % Option.some %
                                       Workout.Sport.of_string)),
-              " Override workout sport" ]
+              " Override workout sport";
+              "-mode", Arg.Symbol ([ "pp"; "tr" ],
+                                   Ref.set mode % (function
+                                       | "pp" -> Repr.to_channel
+                                       | "tr" -> Repr.Il.to_channel
+                                       | _ -> raise (Invalid_argument "mode"))),
+              " Translate or pretty-print" ]
     (fun _anon -> ())
-    "Translate workout description to intermediate language";
-  !name, !sport
+    "Process workout description language";
+  !name, !sport, !mode
 
 let override ?name ?sport =
   (fun workout ->
@@ -25,7 +32,7 @@ let override ?name ?sport =
      else workout)
 
 let () =
-  let name, sport = parse_args () in
+  let name, sport, _to_channel = parse_args () in
   let lexbuf = Lexing.from_channel stdin in
   try
     Repr.from_lexbuf lexbuf |>
