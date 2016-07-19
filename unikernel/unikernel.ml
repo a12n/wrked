5fd65fd1050808df5f02eed7266749d62020a0f5
Anton Yabchinskiy <arn@bestmx.net>
2016-07-19 12:00:53 +0300

    unikernel: parse wrk description wirh Wrk.Repr.from_string

--- unikernel/unikernel.ml
+++ unikernel/unikernel.ml
@@ -2,11 +2,14 @@ module Server_log = (val Logs.src_log (Logs.Src.create "server") : Logs.LOG)
 
 module Main (Server : Cohttp_lwt.Server) = struct
   let callback _id request _body =
-    let path = Cohttp.Request.uri request |> Uri.path in
-    let _wrk = String.(sub path 1 (length path - 1)) |> Uri.pct_decode in
-    (* TODO *)
+    let status =
+      let path = Cohttp.Request.uri request |> Uri.path in
+      let wrk = String.(sub path 1 (length path - 1)) |> Uri.pct_decode in
+      match Wrk.Repr.from_string wrk with
+      | _workout                   -> `OK
+      | exception Wrk.Lexer.Error  -> `Bad_request
+      | exception Wrk.Parser.Error -> `Bad_request in
     let body = "" in
-    let status = `Bad_request in
     Server.respond_string ~status ~body ()
 
   let conn_closed _id =
