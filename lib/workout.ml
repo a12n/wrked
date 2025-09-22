815744fd1b3be95e626f2ffb22f757a18bf17564
Anton Yabchinskiy <arn@bestmx.net>
2025-09-22 17:20:05 +0200

    Workout: ocamlformat

--- lib/workout.ml
+++ lib/workout.ml
@@ -3,58 +3,56 @@ type 'a non_empty_list = 'a * 'a list
 let non_empty_list_to_list (x, xs) = x :: xs
 
 module Capability = struct
-  type t = Speed
-         | Heart_rate
-         | Distance
-         | Cadence
-         | Power
-         | Grade
-         | Resistance
+  type t =
+    | Speed
+    | Heart_rate
+    | Distance
+    | Cadence
+    | Power
+    | Grade
+    | Resistance
 
   (* Values from FIT SDK *)
   let to_int32 = function
-      Speed      -> 0x00000080l
+    | Speed -> 0x00000080l
     | Heart_rate -> 0x00000100l
-    | Distance   -> 0x00000200l
-    | Cadence    -> 0x00000400l
-    | Power      -> 0x00000800l
-    | Grade      -> 0x00001000l
+    | Distance -> 0x00000200l
+    | Cadence -> 0x00000400l
+    | Power -> 0x00000800l
+    | Grade -> 0x00001000l
     | Resistance -> 0x00002000l
 end
 
 module Sport = struct
-  type t = Cycling
-         | Running
-         | Swimming
-         | Walking
+  type t = Cycling | Running | Swimming | Walking
 
   let of_string = function
-      "cycling"  -> Cycling
-    | "running"  -> Running
+    | "cycling" -> Cycling
+    | "running" -> Running
     | "swimming" -> Swimming
-    | "walking"  -> Walking
+    | "walking" -> Walking
     | _ -> invalid_arg "Workout.Sport.of_string"
 
   let to_string = function
-      Cycling  -> "cycling"
-    | Running  -> "running"
+    | Cycling -> "cycling"
+    | Running -> "running"
     | Swimming -> "swimming"
-    | Walking  -> "walking"
+    | Walking -> "walking"
 end
 
-let restricted (min, max) exn =
-  function n when n >= min && n <= max -> n
-         | _ -> raise exn
+let restricted (min, max) exn = function
+  | n when n >= min && n <= max -> n
+  | _ -> raise exn
 
 module Speed = struct
   type t = float
   type zone = int
 
-  let of_float = restricted (1.0, 100.0)
-      (Invalid_argument "Workout.Speed.of_float")
+  let of_float =
+    restricted (1.0, 100.0) (Invalid_argument "Workout.Speed.of_float")
 
-  let zone_of_int = restricted (1, 10)
-      (Invalid_argument "Workout.Speed.zone_of_int")
+  let zone_of_int =
+    restricted (1, 10) (Invalid_argument "Workout.Speed.zone_of_int")
 
   let from_kmph x = x *. 1000.0 /. 3600.0
 end
@@ -63,12 +61,11 @@ module Cadence = struct
   type t = int
   type zone = int
 
-  let of_int = restricted (1, 500)
-      (Invalid_argument "Workout.Cadence.of_int")
+  let of_int = restricted (1, 500) (Invalid_argument "Workout.Cadence.of_int")
 
   (* TODO: Max cadence zone? *)
-  let zone_of_int = restricted (1, 10)
-      (Invalid_argument "Workout.Cadence.zone_of_int")
+  let zone_of_int =
+    restricted (1, 10) (Invalid_argument "Workout.Cadence.zone_of_int")
 end
 
 module Heart_rate = struct
@@ -76,17 +73,16 @@ module Heart_rate = struct
   type percent = int
   type zone = int
 
-  let absolute_of_int = restricted (1, 255)
-      (Invalid_argument "Workout.Heart_rate.absolute_of_int")
+  let absolute_of_int =
+    restricted (1, 255) (Invalid_argument "Workout.Heart_rate.absolute_of_int")
 
-  let percent_of_int = restricted (1, 100)
-      (Invalid_argument "Workout.Heart_rate.percent_of_int")
+  let percent_of_int =
+    restricted (1, 100) (Invalid_argument "Workout.Heart_rate.percent_of_int")
 
-  let zone_of_int = restricted (1, 5)
-      (Invalid_argument "Workout.Heart_rate.zone_of_int")
+  let zone_of_int =
+    restricted (1, 5) (Invalid_argument "Workout.Heart_rate.zone_of_int")
 
-  type t = Absolute of absolute
-         | Percent of percent
+  type t = Absolute of absolute | Percent of percent
 end
 
 module Power = struct
@@ -94,68 +90,68 @@ module Power = struct
   type percent = int
   type zone = int
 
-  let absolute_of_int = restricted (1, 10000)
-      (Invalid_argument "Workout.Power.absolute_of_int")
+  let absolute_of_int =
+    restricted (1, 10000) (Invalid_argument "Workout.Power.absolute_of_int")
 
-  let percent_of_int = restricted (1, 1000)
-      (Invalid_argument "Workout.Power.percent_of_int")
+  let percent_of_int =
+    restricted (1, 1000) (Invalid_argument "Workout.Power.percent_of_int")
 
-  let zone_of_int = restricted (1, 7)
-      (Invalid_argument "Workout.Power.zone_of_int")
+  let zone_of_int =
+    restricted (1, 7) (Invalid_argument "Workout.Power.zone_of_int")
 
-  type t = Absolute of absolute
-         | Percent of percent
+  type t = Absolute of absolute | Percent of percent
 end
 
 module Condition = struct
   type order = Less | Greater
-
   type calories = int
   type distance = int
   type time = int
 
-  let calories_of_int = restricted (1, max_int)
+  let calories_of_int =
+    restricted (1, max_int)
       (Invalid_argument "Workout.Condition.calories_of_int")
 
-  let distance_of_int = restricted (1, max_int)
+  let distance_of_int =
+    restricted (1, max_int)
       (Invalid_argument "Workout.Condition.distance_of_int")
 
-  let time_of_int = restricted (1, max_int)
-      (Invalid_argument "Workout.Condition.time_of_int")
+  let time_of_int =
+    restricted (1, max_int) (Invalid_argument "Workout.Condition.time_of_int")
 
-  type t = Time of time
-         | Distance of distance
-         | Heart_rate of (order * Heart_rate.t)
-         | Calories of calories
-         | Power of (order * Power.t)
+  type t =
+    | Time of time
+    | Distance of distance
+    | Heart_rate of (order * Heart_rate.t)
+    | Calories of calories
+    | Power of (order * Power.t)
 
   let caps = function
-    | Distance _ -> [Capability.Distance]
-    | Heart_rate _ -> [Capability.Heart_rate]
-    | Power _ -> [Capability.Power]
+    | Distance _ -> [ Capability.Distance ]
+    | Heart_rate _ -> [ Capability.Heart_rate ]
+    | Power _ -> [ Capability.Power ]
     | Calories _ | Time _ -> []
 end
 
 module Repeat = struct
   type times = int
 
-  let times_of_int = restricted (2, 1000000)
-      (Invalid_argument "Workout.Repeat.times_of_int")
+  let times_of_int =
+    restricted (2, 1000000) (Invalid_argument "Workout.Repeat.times_of_int")
 
-  type t = Times of times
-         | Until of Condition.t
+  type t = Times of times | Until of Condition.t
 
-  let caps = function
-      Times _ -> []
-    | Until c -> Condition.caps c
+  let caps = function Times _ -> [] | Until c -> Condition.caps c
 end
 
 module Target = struct
-  module Value (S : sig type t type zone end) = struct
-    type range = (S.t * S.t)
-
-    type t = Zone of S.zone
-           | Range of range
+  module Value (S : sig
+    type t
+    type zone
+  end) =
+  struct
+    type range = S.t * S.t
+    type t = Zone of S.zone | Range of range
 
     let range_of_pair (a, b) = if a < b then (a, b) else (b, a)
   end
@@ -165,73 +161,65 @@ module Target = struct
   module Power_value = Value (Power)
   module Speed_value = Value (Speed)
 
-  type t = Speed of Speed_value.t
-         | Heart_rate of Heart_rate_value.t
-         | Cadence of Cadence_value.t
-         | Power of Power_value.t
+  type t =
+    | Speed of Speed_value.t
+    | Heart_rate of Heart_rate_value.t
+    | Cadence of Cadence_value.t
+    | Power of Power_value.t
 
   let caps = function
-      Speed _ -> [Capability.Speed]
-    | Heart_rate _ -> [Capability.Heart_rate]
-    | Cadence _ -> [Capability.Cadence]
-    | Power _ -> [Capability.Power]
+    | Speed _ -> [ Capability.Speed ]
+    | Heart_rate _ -> [ Capability.Heart_rate ]
+    | Cadence _ -> [ Capability.Cadence ]
+    | Power _ -> [ Capability.Power ]
 end
 
 module Intensity = struct
-  type t = Warm_up
-         | Active
-         | Rest
-         | Cool_down
+  type t = Warm_up | Active | Rest | Cool_down
 
   let of_string = function
-      "warmup"   -> Warm_up
-    | "active"   -> Active
-    | "rest"     -> Rest
+    | "warmup" -> Warm_up
+    | "active" -> Active
+    | "rest" -> Rest
     | "cooldown" -> Cool_down
     | _ -> invalid_arg "Workout.Intensity.of_string"
 
   let to_string = function
-      Warm_up   -> "warmup"
-    | Active    -> "active"
-    | Rest      -> "rest"
+    | Warm_up -> "warmup"
+    | Active -> "active"
+    | Rest -> "rest"
     | Cool_down -> "cooldown"
 end
 
 module Step = struct
   type single = {
-    name      : string option;
-    duration  : Condition.t option;
-    target    : Target.t option;
+    name : string option;
+    duration : Condition.t option;
+    target : Target.t option;
     intensity : Intensity.t option;
-  } and repeat = {
-    condition : Repeat.t;
-    steps     : t non_empty_list;
-  } and t = Single of single
-          | Repeat of repeat
+  }
+
+  and repeat = { condition : Repeat.t; steps : t non_empty_list }
+  and t = Single of single | Repeat of repeat
 
   let rec caps = function
-      Single {duration; target; _} ->
-      List.append
-        (match duration with
-         | Some duration -> Condition.caps duration
-         | None          -> [])
-        (match target with
-         | Some target -> Target.caps target
-         | None        -> [])
-    | Repeat {condition; steps} ->
-      List.append
-        (Repeat.caps condition)
-        (non_empty_list_to_list steps |>
-         List.map caps |> List.flatten)
+    | Single { duration; target; _ } ->
+        List.append
+          (match duration with
+          | Some duration -> Condition.caps duration
+          | None -> [])
+          (match target with Some target -> Target.caps target | None -> [])
+    | Repeat { condition; steps } ->
+        List.append (Repeat.caps condition)
+          (non_empty_list_to_list steps |> List.map caps |> List.flatten)
 end
 
 type t = {
-  name  : string option;
+  name : string option;
   sport : Sport.t option;
   steps : Step.t non_empty_list;
 }
 
-let caps {steps; _} =
-  non_empty_list_to_list steps |>
-  List.map Step.caps |> List.flatten |>
-  List.sort_uniq compare
+let caps { steps; _ } =
+  non_empty_list_to_list steps
+  |> List.map Step.caps |> List.flatten |> List.sort_uniq compare
--- lib/workout.mli
+++ lib/workout.mli
@@ -1,28 +1,23 @@
 type 'a non_empty_list = 'a * 'a list
 
 module Sport : sig
-  type t = Cycling
-         | Running
-         | Swimming
-         | Walking
+  type t = Cycling | Running | Swimming | Walking
 
   val of_string : string -> t
-
   val to_string : t -> string
 end
 
 module Speed : sig
-  type t = private float        (* m/s *)
+  type t = private float (* m/s *)
   type zone = private int
 
   val of_float : float -> t
   val zone_of_int : int -> zone
-
   val from_kmph : float -> t
 end
 
 module Cadence : sig
-  type t = private int          (* rpm *)
+  type t = private int (* rpm *)
   type zone = private int
 
   val of_int : int -> t
@@ -30,47 +25,45 @@ module Cadence : sig
 end
 
 module Heart_rate : sig
-  type absolute = private int   (* bpm *)
-  type percent = private int    (* 0-100 % of max *)
-  type zone = private int       (* 1-5 *)
+  type absolute = private int (* bpm *)
+  type percent = private int (* 0-100 % of max *)
+  type zone = private int (* 1-5 *)
 
   val absolute_of_int : int -> absolute
   val percent_of_int : int -> percent
   val zone_of_int : int -> zone
 
-  type t = Absolute of absolute
-         | Percent of percent
+  type t = Absolute of absolute | Percent of percent
 end
 
 module Power : sig
-  type absolute = private int   (* W *)
-  type percent = private int    (* 0-1000 % of FTP *)
-  type zone = private int       (* 1-7 *)
+  type absolute = private int (* W *)
+  type percent = private int (* 0-1000 % of FTP *)
+  type zone = private int (* 1-7 *)
 
   val absolute_of_int : int -> absolute
   val percent_of_int : int -> percent
   val zone_of_int : int -> zone
 
-  type t = Absolute of absolute
-         | Percent of percent
+  type t = Absolute of absolute | Percent of percent
 end
 
 module Condition : sig
   type order = Less | Greater
-
-  type calories = private int   (* kcal *)
-  type distance = private int   (* m *)
-  type time = private int       (* s *)
+  type calories = private int (* kcal *)
+  type distance = private int (* m *)
+  type time = private int (* s *)
 
   val calories_of_int : int -> calories
   val distance_of_int : int -> distance
   val time_of_int : int -> time
 
-  type t = Time of time
-         | Distance of distance
-         | Heart_rate of (order * Heart_rate.t)
-         | Calories of calories
-         | Power of (order * Power.t)
+  type t =
+    | Time of time
+    | Distance of distance
+    | Heart_rate of (order * Heart_rate.t)
+    | Calories of calories
+    | Power of (order * Power.t)
 end
 
 module Repeat : sig
@@ -78,17 +71,18 @@ module Repeat : sig
 
   val times_of_int : int -> times
 
-  type t = Times of times
-         | Until of Condition.t
+  type t = Times of times | Until of Condition.t
 end
 
 module Target : sig
-  module Value : functor (S : sig type t type zone end) ->
-  sig
-    type range = private (S.t * S.t)
-
-    type t = Zone of S.zone
-           | Range of range
+  module Value : functor
+    (S : sig
+       type t
+       type zone
+     end)
+    -> sig
+    type range = private S.t * S.t
+    type t = Zone of S.zone | Range of range
 
     val range_of_pair : S.t * S.t -> range
   end
@@ -98,38 +92,34 @@ module Target : sig
   module Power_value : module type of Value (Power)
   module Speed_value : module type of Value (Speed)
 
-  type t = Speed of Speed_value.t
-         | Heart_rate of Heart_rate_value.t
-         | Cadence of Cadence_value.t
-         | Power of Power_value.t
+  type t =
+    | Speed of Speed_value.t
+    | Heart_rate of Heart_rate_value.t
+    | Cadence of Cadence_value.t
+    | Power of Power_value.t
 end
 
 module Intensity : sig
-  type t = Warm_up
-         | Active
-         | Rest
-         | Cool_down
+  type t = Warm_up | Active | Rest | Cool_down
 
   val of_string : string -> t
-
   val to_string : t -> string
 end
 
 module Step : sig
   type single = {
-    name      : string option;
-    duration  : Condition.t option;
-    target    : Target.t option;
+    name : string option;
+    duration : Condition.t option;
+    target : Target.t option;
     intensity : Intensity.t option;
-  } and repeat = {
-    condition : Repeat.t;
-    steps     : t non_empty_list;
-  } and t = Single of single
-          | Repeat of repeat
+  }
+
+  and repeat = { condition : Repeat.t; steps : t non_empty_list }
+  and t = Single of single | Repeat of repeat
 end
 
 type t = {
-  name  : string option;
+  name : string option;
   sport : Sport.t option;
   steps : Step.t non_empty_list;
 }
@@ -137,13 +127,14 @@ type t = {
 (* {2 Capabilities flags} *)
 
 module Capability : sig
-  type t = Speed
-         | Heart_rate
-         | Distance
-         | Cadence
-         | Power
-         | Grade
-         | Resistance
+  type t =
+    | Speed
+    | Heart_rate
+    | Distance
+    | Cadence
+    | Power
+    | Grade
+    | Resistance
 
   val to_int32 : t -> int32
 end
