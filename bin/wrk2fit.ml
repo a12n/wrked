0522edfa9cb8b0e6544634a1673cd91ae09ebc73
Anton Yabchinskiy <arn@bestmx.net>
2025-09-25 19:38:56 +0200

    bin/wrk2fit: WIP, run in minimize mode

--- bin/wrk2fit.ml
+++ bin/wrk2fit.ml
@@ -1,1 +1,6 @@
-let () = print_endline "TODO"
+let () =
+  match Workout.Parser.parse_channel stdin with
+  | Ok w -> Workout.Printer.Channel.print stdout w
+  | Error msg ->
+      prerr_endline msg;
+      exit 1
