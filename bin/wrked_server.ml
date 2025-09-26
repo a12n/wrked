7751d27a6b5c6b615a9879ad35c767928e66b7a2
Anton Yabchinskiy <arn@bestmx.net>
2025-09-26 09:38:51 +0200

    bin/wrked_server: WIP

--- bin/wrked_server.ml
+++ bin/wrked_server.ml
@@ -1,1 +1,25 @@
-let () = print_endline "TODO"
+let callback _conn req _body =
+  match Http.Request.meth req with
+  | `GET ->
+      (* TODO *)
+      Cohttp_eio.Server.respond_string ~status:`Not_found ~body:"" ()
+  | _ ->
+      Cohttp_eio.Server.respond_string ~status:`Method_not_allowed ~body:"" ()
+
+let on_error _exn =
+  (* TODO *)
+  ()
+
+let main env =
+  let net = Eio.Stdenv.net env in
+  Eio.Switch.run (fun sw ->
+      let backlog = 128 in
+      let reuse_addr = true in
+      let socket =
+        Eio.Net.listen ~sw ~backlog ~reuse_addr net
+          (`Tcp (Eio.Net.Ipaddr.V4.loopback, 8080))
+      in
+      let server = Cohttp_eio.Server.make ~callback () in
+      Cohttp_eio.Server.run ~on_error socket server)
+
+let () = Eio_main.run main
